38c152b996f9eef541aa4d55feb7cb6ef3ab9a9c
Ouzinem <ouzinem@hotmail.com>
2025-03-07 07:45:59 -0500

    refactor: update HTML structure and enhance task management UI with a new Checkbox component

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,46 @@
 <template>
-  <form action="" @submit.prevent="addTask">
-    <input type="text" placeholder="add a new task" v-model="newTask" /><button
-      :disabled="newTask.length === 0"
-    >
-      Add a taks
-    </button>
-  </form>
+  <div style="margin-block: 2rem">
+    <form action="" @submit.prevent="addTask">
+      <fieldset role="group">
+        <label for=""></label>
+        <input type="text" placeholder="add a new task" v-model="newTask" /><button
+          :disabled="newTask.length === 0"
+        >
+          Add
+        </button>
+      </fieldset>
+    </form>
 
-  <div>
-    <div v-if="!tasks.length">
-      <p>Vous n'avez pas de taches</p>
-    </div>
+    <div>
+      <div v-if="!tasks.length">
+        <p>Vous n'avez pas de taches</p>
+      </div>
 
-    <div v-else>
-      <ul>
-        <li v-for="task in sortedTasks" :key="task.date" :class="{ completed: task.completed }">
-          <label>
-            <input type="checkbox" v-model="task.completed" v-bind:id="task.title" />
-          </label>
-          {{ task.title }}
-        </li>
-      </ul>
+      <div v-else>
+        <ul>
+          <li v-for="task in sortedTasks" :key="task.date" :class="{ completed: task.completed }">
+            <Checkbox
+              :label="task.title"
+              @check="console.log('coche')"
+              @uncheck="console.log('decoche')"
+              v-model="task.completed"
+              v-bind:id="task.title"
+            />
+          </li>
+        </ul>
 
-      <label> <input type="checkbox" v-model="hideCompleted" /> hide completed tasks </label>
-      <p v-if="remainingTask > 0">
-        {{ remainingTask }} tache{{ remainingTask > 1 ? "s" : "" }} a faitre
-      </p>
+        <label> <input type="checkbox" v-model="hideCompleted" /> hide completed tasks </label>
+        <p v-if="remainingTask > 0">
+          {{ remainingTask }} tache{{ remainingTask > 1 ? "s" : "" }} a faitre
+        </p>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref, reactive, computed } from "vue";
+import { ref, computed } from "vue";
+import Checkbox from "./components/Checkbox.vue";
 
 const newTask = ref("");
 
@@ -67,7 +76,7 @@ const addTask = () => {
 };
 
 const sortedTasks = computed(() => {
-  console.log("ici");
+  // console.log("ici");
   const sortedTasks = tasks.value.toSorted((a, b) => (a.completed > b.completed ? 1 : -1));
 
   if (hideCompleted.value === true) {
@@ -81,11 +90,22 @@ const remainingTask = computed(() => {
 });
 
 console.log(remainingTask.value);
+
+const tonText = ref(["ici", "allo"]);
+const affiche = computed(() => {
+  return tonText.value.map((element) => {
+    return element;
+  });
+});
+console.log(affiche.value);
 </script>
 
 <style>
 .completed {
   opacity: 0.5;
   text-decoration: line-through;
+}
+ul li {
+  list-style: none;
 }
 </style>
---
+++ src/components/Checkbox.vue
@@ -0,0 +1,23 @@
+<template>
+  <label for=""> <input type="checkbox" @change="onChange" v-model="model" /> {{ label }} </label>
+</template>
+
+<script setup>
+// pour faire descendre l'information du parent vers enfant
+defineProps({
+  label: String,
+});
+
+const model = defineModel();
+const emits = defineEmits(["check", "uncheck"]);
+
+const onChange = (event) => {
+  if (event.currentTarget.checked) {
+    emits("check", event.currentTarget);
+  } else {
+    emits("uncheck", event.currentTarget);
+  }
+};
+
+// console.log(label);
+</script>
