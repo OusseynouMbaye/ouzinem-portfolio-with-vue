08c1d7dff269c89d0c643d171f64409b18bd5ec4
Ouzinem <ouzinem@hotmail.com>
2025-03-15 07:03:01 -0400

    Add NavBar component and implement mobile toggle functionality

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <template>
+  <NavBar />
   <h1 class="text-xl font-bold underline bg-primary">Hello world!</h1>
   <p class="bg-secondary p-4">
     Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto, natus.
   </p>
 </template>
 
-<script setup></script>
+<script setup>
+import NavBar from './components/layout/NavBar.vue';
+</script>
 <style scoped></style>
--- src/components/layout/NavBar.vue
+++ src/components/layout/NavBar.vue
@@ -4,10 +4,30 @@
       <div class="text-3xl font-bold dark:text-white">My Logo Here</div>
       <!-- Mobile toggle Button -->
       <div class="md:hidden z-30">
-        <button class="block focus:outline-none"></button>
+        <button
+          class="block focus:outline-none"
+          @click="isMenuOpen = !isMenuOpen"
+        >
+          <span
+            v-if="isMenuOpen"
+            class="text-5xl md:text-primary text-white dark:texte-white"
+          >
+            <Icon icon="material-symbols:close" />
+          </span>
+          <span
+            v-else
+            class="text-5xl md:text-primary text-white dark:texte-white"
+          >
+            <Icon icon="material-symbols:menu" />
+          </span>
+        </button>
       </div>
     </div>
   </header>
 </template>
-<script setup></script>
+<script setup>
+import { ref } from 'vue';
+
+const isMenuOpen = ref(false);
+</script>
 <style lang=""></style>
