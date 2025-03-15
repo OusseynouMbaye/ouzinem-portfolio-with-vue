2863929176c77b68525d78604ca139d3ceae65c5
Ouzinem <ouzinem@hotmail.com>
2025-03-15 06:39:58 -0400

    start navbar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
-  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  <h1 class="text-xl font-bold underline bg-primary">Hello world!</h1>
+  <p class="bg-secondary p-4">
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto, natus.
+  </p>
 </template>
 
 <script setup></script>
---
+++ src/components/layout/NavBar.vue
@@ -0,0 +1,13 @@
+<template>
+  <header>
+    <div class="flex justify-center items-center p-8 lg:px-12 relative z-20">
+      <div class="text-3xl font-bold dark:text-white">My Logo Here</div>
+      <!-- Mobile toggle Button -->
+      <div class="md:hidden z-30">
+        <button class="block focus:outline-none"></button>
+      </div>
+    </div>
+  </header>
+</template>
+<script setup></script>
+<style lang=""></style>
